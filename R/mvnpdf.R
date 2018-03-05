59809ee6bd7b206897490f007bc1f9ba82dea421
cpasin <36972373+cpasin@users.noreply.github.com>
2018-03-05 14:08:25 +0100

    1ere version formation R

---
+++ R/mvnpdf.R
@@ -0,0 +1,34 @@
+#' Fonction mvnpdf
+#'
+#' calcule la valeur de la densité d’une loi normale multivariée sur Rp en n points
+#'
+#' @param x : matrice d'observations en colonnes (n colonnes, p lignes)
+#' @param mean : vecteur de moyenne
+#' @param varcovM : matrice de variance covariance
+#' @param Log : TRUE par défaut (calcul du log)
+#'
+#' @return matrice x et vecteur y de la valeur de la densité en chacun des vecteurs colonne de x
+#' @export
+#'
+#' @examples
+mvnpdf <- function(x, mean =  rep(0, nrow(x)),
+                   varcovM = diag(nrow(x)), Log = TRUE) {
+  n <- ncol(x)
+  p <- nrow(x)
+  x0 <- x - mean
+  Rinv <- solve(varcovM)
+  LogDetvarcovM <- log(det(varcovM))
+
+  y <- NULL
+  for (j in 1:n) {
+    yj <- - p/2 * log(2*pi) - 0.5 * LogDetvarcovM -
+      0.5 * t(x0[, j]) %*% Rinv %*% x0[, j]
+    y <- c(y, yj)
+  }
+
+  if (!Log) {
+    y <- exp(y)
+  }
+
+  return(list(x=x,y=y))
+}
---
+++ R/plotmvnpdf.R
@@ -0,0 +1,16 @@
+#' Plot of the mvnpdf function
+#'
+#' @param x an object of class \code{mvnpdf} resulting from a call of
+#' \code{mnvpdf()} function.
+#' @param ... graphical parameters passed to \code{plot()} function.
+#'
+#' @return Nothing is returned, only a plot is given.
+#' @export
+#' @importFrom graphics plot
+#'
+#' @examples
+#' pdfvalues <- mvnpdf(x=matrix(seq(-3, 3, by = 0.1), nrow = 1), Log=FALSE)
+#' plot(pdfvalues)
+plot.mvnpdf <- function(x, ...) {
+  plot(x$x, x$y, type = "l", ...)
+}
